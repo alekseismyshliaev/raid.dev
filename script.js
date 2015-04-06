fcc9b4d0025647f60f6925d3d15a46d13065af0b
alexey <smishlayev@gmail.com>
2015-04-06 16:06:08 +0300

    added data loss example for RAID 5

--- script.js
+++ script.js
@@ -715,6 +715,66 @@ function raid_5(h, v, d, dd)
 			$(this).text('READ');
 		}
 	})
+
+	$('#dataloss', '#raid-5').click(function()
+	{
+		if($('#start', '#raid-5').text() == 'READ')
+		{
+			var horizontal = h;
+			var vertical = v;
+			var delay = d;
+			var dataDelay = dd+50;
+			var lostData = $('.node:nth-child(5n+1)', '#raid-5');
+			var lostDisk = $('.HDD:first', '#raid-5');
+			var diskTitle = $('.HDD-title:first', '#raid-5');
+			
+			lostData.css('opacity', '0.0');
+			lostDisk.css('opacity', '0.0');
+			diskTitle.text('Lost');
+			setTimeout(function()
+			{
+				lostDisk.hide();
+				lostDisk.css('opacity', '1.0');
+				lostDisk.slideDown(1000, function()
+ 				{
+					$(diskTitle).text('New disk');
+				});
+			}, 1000);
+			setTimeout(function(){
+				$(lostData[0]).css('opacity', '1.0');
+			}, 4000);
+			setTimeout(function(){
+				$(lostData[1]).css('opacity', '1.0');
+			}, 8000);
+			setTimeout(function(){
+				$(lostData[2]).css('opacity', '1.0');
+			}, 12500);
+			setTimeout(function(){
+				$(lostData[3]).css('opacity', '1.0');
+			}, 18000);
+			setTimeout(function(){
+				$(lostData[4]).css('opacity', '1.0');
+			}, 22500);
+			for(var ii=0; ii<nodes.length / 5; ii+=1)
+			{
+				currentNodes = nodes.slice(ii*5, ii*5+5);
+				$.each(currentNodes, function( index, value )
+				{
+					runReadNode(value, 0, 0, delay);
+					delay += 500;
+				});
+				delay += 2000;
+				var lostElement = $(lostData[ii]);
+				runNode(lostElement, delay, horizontal, vertical);
+				vertical += 26;
+				delay += 500;
+			}
+			setTimeout(function()
+			{
+				diskTitle.text('Disk 1');
+			}, 27000);
+		}
+	})
 }
 
 function raid_6(h, v, d, dd)
